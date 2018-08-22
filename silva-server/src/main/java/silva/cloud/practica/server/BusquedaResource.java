337a03b4b996c5c73733bd6ce284fd6614fa464e
zullysr <42480348+zullysr@users.noreply.github.com>
2018-08-22 17:20:42 -0500

    Update BusquedaResource.java

--- silva-server/src/main/java/silva/cloud/practica/server/BusquedaResource.java
+++ silva-server/src/main/java/silva/cloud/practica/server/BusquedaResource.java
@@ -1,7 +1,7 @@
 package silva.cloud.practica.server;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
